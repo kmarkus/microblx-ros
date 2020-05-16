b360b0de62bcef924177ec6f4e6be2183fa247b1
Markus Klotzbuecher <mk@mkio.de>
2020-05-16 08:54:40 +0200

    wip: skelleton of high-level multi-type block building
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- src/ubxros.cpp
+++ src/ubxros.cpp
@@ -10,28 +10,75 @@
 #include "types/ubxros_conn.h"
 #include "types/ubxros_conn.h.hexarr"
 
-/* types */
+// types
 #include "ros_kdl.hpp"
 #include "ros_std.hpp"
 
-/* block meta information */
+// ubxros handlers
+
+typedef std::function<void ()> pubFunc;
+
+// this is used to declare the type that ubxros can handle for now
+// it's just a static compile time list. Could be extended to with
+// dynamic loadable plugins.
+
+struct ubxros_handler {
+    const char *ros_type;
+    const char *ubx_type;
+
+    // a factory for creating a Subscription to the topic described by
+    // uc. Will setup a callback for publishing received values on
+    // port p_sub
+    ros::Subscriber (*subfact)(ros::NodeHandle *nh,
+                               const struct ubxros_conn *uc,
+                               const ubx_port_t *p_sub);
+
+
+    // a factory for creating publisher functions. A publisher
+    // function is a thunk that will read from the given port and
+    // publish it onto the given topic.
+    pubFunc (*pubfact)(ros::NodeHandle *nh,
+                       const struct ubxros_conn *uc,
+                       const ubx_port_t *p_pub);
+
+};
+
+//
+// Block definitions
+//
+
+// block meta information
 static char ubxros_meta[] =
-    " { doc='A bridge block to connect to ROS', realtime=false }";
+    "{ doc='A generic mult-type bridge block to connect to ROS', realtime=false }";
 
 
-#define CONNECTIONS	"connections"
+const char* CONNECTIONS = "connections";
+
 /* declaration of block configuration */
-static ubx_config_t ros_config[] = {
+static ubx_config_t ubxros_config[] = {
     { .name=CONNECTIONS, .doc="microblx-ROS connections", .type_name = "struct ubxros_conn" },
     { 0 },
 };
 
-/* types defined by ptrig block */
+// ubxros types and accessors
 ubx_type_t ubxros_types[] = {
-	def_struct_type(struct ubxros_conn, &ubxros_conn_h),
-	{ 0 },
+    def_struct_type(struct ubxros_conn, &ubxros_conn_h),
+    { 0 },
 };
 
+def_cfg_getptr_fun(cfg_getptr_ubxros_conn, struct ubxros_conn);
+
+
+// Block state
+struct ubxros_info
+{
+    ros::NodeHandle *nh;
+    const struct ubxros_conn *conns; /* ROS connections */
+};
+
+
+
+// Concrete type handers
 ros::Subscriber makeInt32Sub(ros::NodeHandle *nh,
                              const struct ubxros_conn *uc,
                              const ubx_port_t *p_sub)
@@ -45,50 +92,39 @@ ros::Subscriber makeInt32Sub(ros::NodeHandle *nh,
         });
 }
 
-ros::Publisher makeInt32Pub(ros::NodeHandle *nh,
-                            const struct ubxros_conn *uc,
-                            const ubx_port_t *p_pub)
+pubFunc makeInt32Pub(ros::NodeHandle *nh,
+                     const struct ubxros_conn *uc,
+                     const ubx_port_t *p_pub)
 {
-    return nh->advertise<std_msgs::Int32>(uc->topic, uc->queue_size, uc->latch);
+    ros::Publisher pub = nh->advertise<std_msgs::Int32>(uc->topic, uc->queue_size, uc->latch);
+
+    return [&p_pub,pub]() {
+               std_msgs::Int32 msg;
+               int len = portRead(p_pub, &msg.data, 1);
+
+               if(len > 0)
+                   pub.publish(msg);
+               else if (len == 0)
+                   ubx_debug(p_pub->block, "no new data");
+               else
+                   ubx_err(p_pub->block, "failed to read port %s", p_pub->name);
+           };
 }
 
 
-/* this is used to declare the type that ubxros can handle
- * for now it's just a static compile time list. Could be extended to
- * with dynamic loadable plugins..
- */
-struct ubxros_handler {
-    char *ros_type;
-    char *ubx_type;
-
-    ros::Subscriber (*subfact)(ros::NodeHandle *nh,
-                               const struct ubxros_conn *uc,
-                               const ubx_port_t *p_sub);
-
-    ros::Publisher (*pubfact)(ros::NodeHandle *nh,
-                              const struct ubxros_conn *uc,
-                              const ubx_port_t *p_pub);
-};
-
-struct ubxros_handler handlers [] =
-{
+struct ubxros_handler handlers [] = {
     {
-        .ros_type = "std_msgs/Int32", .ubx_type = "int32_t",
-        .subfact = makeInt32Sub, .pubfact = makeInt32Pub
+        .ros_type = "std_msgs/Int32",
+        .ubx_type = "int32_t",
+        .subfact = makeInt32Sub,
+        .pubfact = makeInt32Pub
     },
 };
 
 
 
-/* block state */
-struct ubxros_info
-{
-    ros::NodeHandle *nh;
 
-    struct ubxros_conn *conns; /* ROS connections */
-};
-
-static int ros_init(ubx_block_t *b)
+static int ubxros_init(ubx_block_t *b)
 {
     int ret = -1;
     long len;
@@ -105,13 +141,11 @@ static int ros_init(ubx_block_t *b)
 
     b->private_data = inf;
 
-    len = cfg_getptr_ubxros_conns(CONNECTIONS, &inf->conns);
+    len = cfg_getptr_ubxros_conn(b, CONNECTIONS, &inf->conns);
     assert(len>0);
 
-    if (len == 0) {
+    if (len == 0)
         ubx_warn(b, "config connections unset");
-        goto out_free;
-    }
 
     /* need to create node? */
     if (!ros::isInitialized()) {
@@ -145,36 +179,37 @@ out:
 }
 
 /* start */
-static int ros_start(ubx_block_t *b)
+static int ubxros_start(ubx_block_t *b)
 {
-    long len;
-
-    struct ros_info *inf = (struct ros_info*) b->private_data;
+    // struct ubxros_info *inf = (struct ubxros_info*) b->private_data;
 
     if (!ros::ok()) {
         ubx_err(b, "ROS node not initialized or shutting down");
         return -1;
     }
 
-    /* create publishers and subscribers
+    // create publishers and subscribers
 
-    /* OK */
+    // OK
     return 0;
 }
 
 /* stop */
-static void ros_stop(ubx_block_t *b)
+static void ubxros_stop(ubx_block_t *b)
 {
-    struct ros_info *inf = (struct ros_info*) b->private_data;
+    (void)b;
+
+    //struct ubxros_info *inf = (struct ubxros_info*) b->private_data;
+    ubx_debug(b, "shutting down");
     // ubx_debug(b, "shutting down %s", inf->topic);
     // inf->pub.shutdown();
     // inf->sub.shutdown();
 }
 
 /* cleanup */
-static void ros_cleanup(ubx_block_t *b)
+static void ubxros_cleanup(ubx_block_t *b)
 {
-    struct ros_info *inf = (struct ros_info*) b->private_data;
+    struct ubxros_info *inf = (struct ubxros_info*) b->private_data;
 
     ubx_info(b, "shutting down ROS node %s", ros::this_node::getName().c_str());
 
@@ -187,26 +222,17 @@ static void ros_cleanup(ubx_block_t *b)
 }
 
 /* step */
-static void ros_step(ubx_block_t *b)
+static void ubxros_step(ubx_block_t *b)
 {
-    long len;
-    T msg;
+    (void)b;
 
-    struct ros_info *inf = (struct ros_info*) b->private_data;
+    // struct ubxros_info *inf = (struct ubxros_info*) b->private_data;
 
     // process callbacks
     ros::spinOnce();
 
     // publish data
-    len = portRead(inf->p_pub, &msg.data);
-
-    if(len > 0) {
-        inf->pub.publish(msg);
-    } else if (len == 0) {
-        ubx_debug(b, "no new data");
-    } else {
-        ubx_err(b, "failed to read toros port");
-    }
+    // TODO invoke all publishers
 }
 
 /* put everything together */
@@ -215,17 +241,20 @@ ubx_block_t ubxros_block =
     .name = "ubxros",
     .meta_data = ubxros_meta,
     .type = BLOCK_TYPE_COMPUTATION,
-    .configs = ros_config,
-    .init = ros_init,
-    .start = ros_start,
-    .stop = ros_stop,
-    .cleanup = ros_cleanup,
-    .step = ros_step,
+    .configs = ubxros_config,
+    .init = ubxros_init,
+    .start = ubxros_start,
+    .stop = ubxros_stop,
+    .cleanup = ubxros_cleanup,
+    .step = ubxros_step,
 };
 
 
 int ubxros_mod_init(ubx_node_info_t* ni)
 {
+    int ret;
+    ubx_type_t *tptr;
+
     for (tptr = ubxros_types; tptr->name != NULL; tptr++) {
         ret = ubx_type_register(ni, tptr);
         if (ret != 0) {
@@ -235,10 +264,10 @@ int ubxros_mod_init(ubx_node_info_t* ni)
             goto out;
         }
     }
-    ret = ubx_block_register(ni, ubxros_block);
+    ret = ubx_block_register(ni, &ubxros_block);
     if (ret != 0) {
         ubx_log(UBX_LOGLEVEL_ERR, ni, __func__,
-                "failed to register %s block", ubxros_block->name);
+                "failed to register %s block", ubxros_block.name);
     }
 
 out:
@@ -249,10 +278,10 @@ void ubxros_mod_cleanup(ubx_node_info_t *ni)
 {
     ubx_type_t *tptr;
 
-    for (tptr = ptrig_types; tptr->name != NULL; tptr++)
+    for (tptr = ubxros_types; tptr->name != NULL; tptr++)
         ubx_type_unregister(ni, tptr->name);
 
-    ubx_block_unregister(ni, ubxros_block->name);
+    ubx_block_unregister(ni, ubxros_block.name);
 }
 
 UBX_MODULE_INIT(ubxros_mod_init)
